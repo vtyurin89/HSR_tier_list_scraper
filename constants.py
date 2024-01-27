85877607a3fada3736d3eaeb6864303860002d1d
Владимир Тюрин <vlad89main@gmail.com>
2024-01-27 19:07:09 +0300

    Version 1.0

---
+++ constants.py
@@ -0,0 +1,5 @@
+
+# Main constants
+URL = 'https://www.prydwen.gg/star-rail/tier-list/'
+URL_ROOT = 'https://www.prydwen.gg'
+CHARACTER_URL_FIRST_PART = '/star-rail/characters/'
--- parse_sync.py
+++ parse_sync.py
@@ -1,17 +1,18 @@
 import requests
 import csv
-import json
-import time
 from bs4 import BeautifulSoup
 
+
 from models import Character
-
-URL = 'https://www.prydwen.gg/star-rail/tier-list/'
-URL_ROOT = 'https://www.prydwen.gg'
-CHARACTER_URL_FIRST_PART = '/star-rail/characters/'
+from constants import URL, URL_ROOT, CHARACTER_URL_FIRST_PART
+from utils import calculate_time_decorator
 
 
-class HSRParser:
+# TODO
+# FIX parser UnicodeEncodeError
+
+
+class ParserBaseclass:
     """
     This class was designed for website prydwen.gg.
     """
@@ -21,26 +22,8 @@ class HSRParser:
         self.character_url_first_part = character_url_first_part
 
     @staticmethod
-    def _calculate_time_decorator(func):
-        def wrapper(*args, **kwargs):
-            start_time = time.time()
-            result = func(*args, **kwargs)
-            end_time = time.time()
-            print(f"CHECKING TIME ==>> Process finished in: {end_time - start_time}")
-            return result
-        return wrapper
-
-    @_calculate_time_decorator
-    def parse(self) -> None:
-        self.create_csv()
-        soup = BeautifulSoup(requests.get(url=self.url).text, 'lxml')
-        character_divs = soup.findAll("div", class_='avatar-card card')
-        for character_div in character_divs:
-            character_url = URL_ROOT + character_div.find('a').attrs.get('href')
-            character_name = character_url.split(CHARACTER_URL_FIRST_PART)[1]
-            self._create_character(character_name, character_url)
-            self._parse_individual_character(character_url, character_name)
-        self.write_in_csv()
+    def _clean_string(input_str):
+        return input_str.encode('ascii', 'ignore').decode('utf-8')
 
     def _create_character(self, character_name: str, character_url: str) -> None:
         self.characters.setdefault(character_name, Character(
@@ -69,19 +52,6 @@ class HSRParser:
         return {item.find('div', class_="stats-header").text.strip():
                 [stat.text for stat in item.findAll('div', class_='hsr-stat')]
                 for item in stats_divs}
-
-    def _parse_individual_character(self, character_url: str, character_name: str) -> None:
-        soup = BeautifulSoup(requests.get(url=character_url).text, 'lxml')
-        self.characters[character_name].real_name = soup.find('div', class_='character-top').find('strong').text
-        self.characters[character_name].element = soup.find('div', class_='character-top').\
-            find('strong').attrs.get('class')[0]
-        relic_divs = soup.find('div', class_='relics row row-cols-xxl-2 row-cols-xl-2 row-cols-1').\
-            findChildren('div', class_='col')
-        relic_items_div = relic_divs[0].findAll("div", class_='relic-sets-rec')
-        planetary_items_div = relic_divs[1].findAll("div", class_='relic-sets-rec')
-        self.characters[character_name].best_relic_sets = self._get_relics(relic_items_div)
-        self.characters[character_name].best_planetary_sets = self._get_relics(planetary_items_div)
-        self.characters[character_name].best_stats = self._get_best_stats(soup)
 
     @staticmethod
     def create_csv():
@@ -116,9 +86,39 @@ class HSRParser:
                     print('==================================================================================')
 
 
+class HSRParserSync(ParserBaseclass):
+
+    def __init__(self, url: str, character_url_first_part: str):
+        super().__init__(url, character_url_first_part)
+
+    @calculate_time_decorator
+    def parse(self) -> None:
+        self.create_csv()
+        soup = BeautifulSoup(requests.get(url=self.url).text, 'lxml')
+        character_divs = soup.findAll("div", class_='avatar-card card')
+        for character_div in character_divs:
+            character_url = URL_ROOT + character_div.find('a').attrs.get('href')
+            character_name = character_url.split(CHARACTER_URL_FIRST_PART)[1]
+            self._create_character(character_name, character_url)
+            self._parse_individual_character(character_url, character_name)
+        self.write_in_csv()
+
+    def _parse_individual_character(self, character_url: str, character_name: str) -> None:
+        soup = BeautifulSoup(requests.get(url=character_url).text, 'lxml')
+        self.characters[character_name].real_name = soup.find('div', class_='character-top').find('strong').text
+        self.characters[character_name].element = soup.find('div', class_='character-top').\
+            find('strong').attrs.get('class')[0]
+        relic_divs = soup.find('div', class_='relics row row-cols-xxl-2 row-cols-xl-2 row-cols-1').\
+            findChildren('div', class_='col')
+        relic_items_div = relic_divs[0].findAll("div", class_='relic-sets-rec')
+        planetary_items_div = relic_divs[1].findAll("div", class_='relic-sets-rec')
+        self.characters[character_name].best_relic_sets = self._get_relics(relic_items_div)
+        self.characters[character_name].best_planetary_sets = self._get_relics(planetary_items_div)
+        self.characters[character_name].best_stats = self._get_best_stats(soup)
+
+
 if __name__ == "__main__":
-    my_parser = HSRParser(URL, CHARACTER_URL_FIRST_PART)
+    my_parser = HSRParserSync(URL, CHARACTER_URL_FIRST_PART)
     my_parser.parse()
-    print(my_parser.characters)
 
 
